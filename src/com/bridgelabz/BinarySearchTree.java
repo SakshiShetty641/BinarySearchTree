d6bfea4000a9734251b0b65de29c6d2c395c9fca
Sakshi <sakshishetty1231997@gmail.com>
2021-08-24 12:25:20 +0530

    Added: To create Binary tree with more values

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 /**
- * Purpose - To create Binary Search Tree
+ * Purpose - To create Binary Search Tree and add more values
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-23
@@ -12,6 +12,16 @@ public class BinarySearchTree {
         myTree.add(56);
         myTree.add(30);
         myTree.add(70);
+        myTree.add(22);
+        myTree.add(40);
+        myTree.add(11);
+        myTree.add(3);
+        myTree.add(16);
+        myTree.add(60);
+        myTree.add(95);
+        myTree.add(65);
+        myTree.add(63);
+        myTree.add(67);
         myTree.print(myTree.root);
     }
 }
