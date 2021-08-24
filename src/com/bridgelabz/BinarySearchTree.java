e6748665e05c9485a4ba589d6825436f23f98686
Sakshi <sakshishetty1231997@gmail.com>
2021-08-24 08:08:43 +0530

    Added: To create a Binary Search Tree

--- src/com/bridgelabz/BinarySearchTree.java
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -1,6 +1,6 @@
 package com.bridgelabz;
 /**
- * Purpose - To implement Binary Search Tree Operations
+ * Purpose - To create Binary Search Tree
  * @author - Sakshi Shetty
  * @version - 16.0
  * @since - 2021-08-23
@@ -8,5 +8,10 @@ package com.bridgelabz;
 public class BinarySearchTree {
     public static void main(String[] args) {
         System.out.println("Welcome to the Program");
+        BinaryTree<Integer> myTree = new BinaryTree<>();
+        myTree.add(56);
+        myTree.add(30);
+        myTree.add(70);
+        myTree.print(myTree.root);
     }
 }
