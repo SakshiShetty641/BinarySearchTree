903341a3ff84e042487af26d6bc6d9352025a225
Sakshi <sakshishetty1231997@gmail.com>
2021-08-24 07:57:24 +0530

    Added: Welcome message to the program

---
+++ src/com/bridgelabz/BinarySearchTree.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+/**
+ * Purpose - To implement Binary Search Tree Operations
+ * @author - Sakshi Shetty
+ * @version - 16.0
+ * @since - 2021-08-23
+ */
+public class BinarySearchTree {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Program");
+    }
+}
